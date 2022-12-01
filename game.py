92738d69633a7262ac855901aaaede1ea826e734
Jihoon Yoon <jyoon72@my.bcit.ca>
2022-11-30 22:29:58 -0800

    Update validate_move function

--- game.py
+++ game.py
@@ -26,8 +26,8 @@ WATER = ((0, 0), (0, 1), (0, 2), (0, 3), (0, 5), (0, 6), (1, 0), (1, 1), (1, 2),
 OAKRIDGE = ((5, 3), (5, 4), (5, 5), (5, 6), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (7, 3), (7, 4), (7, 5), (7, 6))
 UBC = ((3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (4, 0), (4, 1), (4, 2), (4, 3), (4, 4), (5, 1), (5, 2))
 DOWNTOWN = ((1, 4), (1, 5), (2, 3), (2, 4), (2, 5), (2, 6), (3, 4), (3, 5), (3, 6), (4, 4), (4, 5), (4, 6))
-LOCATIONS = ((0, 7), (1, 4), (1, 5), (2, 5), (2, 3), (3, 2), (3, 3), (3, 4), (4, 6), (5, 6), (6, 4), (6, 6), (6, 8), (8, 4),
-             (9, 2), (9, 3))
+LOCATIONS = ((0, 7), (1, 4), (1, 5), (2, 5), (2, 3), (3, 2), (3, 3), (3, 4), (4, 6), (5, 6), (6, 4), (6, 6), (6, 8),
+             (8, 4), (9, 2), (9, 3))
 
 
 
@@ -133,11 +133,19 @@ def move(character, x_movement, y_movement):
     return character
 
 def validate_move(board, character, direction):
-    if character["position"] in WATER:
-        return False
-    elif character["position"] in UBC and character["level"] < 2:
-        return False
-    elif character["position"] in DOWNTOWN and character["level"] < 3:
+    x_position, y_position = character["position"]
+    if direction == "W":
+        new_position = (x_position, y_position + 1)
+    elif direction == "A":
+        new_position = (x_position - 1, y_position)
+    elif direction == "S":
+        new_position = (x_position, y_position - 1)
+    else:
+        new_position = (x_position + 1, y_position)
+
+    if (new_position in WATER) or\
+            (new_position in UBC and character["level"] < 2) or\
+            (new_position in DOWNTOWN and character["level"] < 3):
         return False
     else:
         return True
