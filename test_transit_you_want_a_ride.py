c03e45b30b8376498e716be6c3c5b63c8979fffd
Elijah_Fabon <113316563+Elijah-Fabon@users.noreply.github.com>
2022-12-04 01:46:51 -0800

    Add unit test suite for you_want_a_ride function in transit.py

--- test_transit_you_want_a_ride.py
+++ test_transit_you_want_a_ride.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
+from unittest.mock import patch
+from transit import you_want_a_ride
 
 
-class Test(TestCase):
-    def test_you_want_a_ride(self):
-        self.fail()
+class TestYouWantARide(TestCase):
+    @patch('builtins.input', return_value='1')
+    def test_you_want_a_ride_yes(self, mock_input):
+        self.assertEqual(you_want_a_ride(), True)
+
+    @patch('builtins.input', return_value='2')
+    def test_you_want_a_ride_no(self, mock_input):
+        self.assertEqual(you_want_a_ride(), False)
